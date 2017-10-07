07536b8b0ad76987546fa0cd9fa30a3933eb9a10
George V. Neville-Neil <gnn@neville-neil.com>
2017-10-07 00:19:28 +0000

    Create a BBN range specific VM build script

---
+++ scripts/cadets-bbn-vm.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+OUTPUT_IMG_NAME=cadets-bbn-vm.qcow2
+
+sudo rm -fr ufs > /dev/null 2>&1 || true
+sudo chflags -R noschg ufs > /dev/null 2>&1 || true
+sudo rm -fr ufs
+sudo mkdir ufs
+for f in base kernel tests
+do
+	sudo tar Jxf ${f}.txz -C ufs
+done
+
+sudo cp /etc/resolv.conf ufs/etc
+
+# workaround the current tarball has no /etc/passwd can causes pkg fail.
+sudo chroot ufs pwd_mkdb -p /etc/master.passwd
+
+sudo chroot ufs env ASSUME_ALWAYS_YES=yes pkg update
+sudo chroot ufs pkg install -y kyua perl5 pdksh kafka git
+
+cat <<EOF | sudo tee ufs/etc/fstab
+# Device        Mountpoint      FStype  Options Dump    Pass#
+/dev/vtbd0p3	/               ufs     rw      1       1
+fdesc           /dev/fd         fdescfs rw      0       0
+EOF
+
+cat <<EOF | sudo tee ufs/boot/loader.conf
+virtio_console_load="YES"
+EOF
+
+cat <<EOF | sudo tee ufs/etc/rc.conf
+hostname="cadets-bbn"
+ifconfig_vtnet0="DHCP"
+ifconfig_vtnet1="DHCP"
+ifconfig_vtnet2="DHCP"
+ifconfig_vtnet3="DHCP"
+sshd_enable="YES"
+ntpd_enable="YES"
+EOF
+
+sudo rm -f ufs/etc/resolv.conf
+
+sudo makefs -d 6144 -t ffs -f 200000 -s 2g -o version=2,bsize=32768,fsize=4096 -Z ufs.img ufs
+mkimg -s gpt -f qcow2 \
+	-b ufs/boot/pmbr \
+	-p freebsd-boot:=ufs/boot/gptboot \
+	-p freebsd-swap/swapfs::1G \
+	-p freebsd-ufs/rootfs:=ufs.img \
+	-o ${OUTPUT_IMG_NAME}
+xz -f -0 ${OUTPUT_IMG_NAME}
