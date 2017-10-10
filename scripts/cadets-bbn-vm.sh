6a8763167aca6e6a9a52c29b51467b00d0b49646
George Neville-Neil <gnn@neville-neil.com>
2017-10-10 19:49:11 -0400

    Add darpa user and pip packages

--- scripts/cadets-bbn-vm.sh
+++ scripts/cadets-bbn-vm.sh
@@ -17,7 +17,16 @@ sudo cp /etc/resolv.conf ufs/etc
 sudo chroot ufs pwd_mkdb -p /etc/master.passwd
 
 sudo chroot ufs env ASSUME_ALWAYS_YES=yes pkg update
-sudo chroot ufs pkg install -y kyua perl5 pdksh kafka git python3 py36-pip
+sudo chroot ufs pkg install -y sudo bash alpine autoconf automake avro-cpp \
+     bison cmake coreutils curl git gmake htop jansson jq jsoncpp kafkacat \
+     librdkafka libtool links m4 maven33 nginx ninja openjdk8 php56 postfix \
+     postgresql95-client postgresql95-server postgresql95-contrib tmux vim \
+     emacs25 wget zsh python3 py36-setuptools
+
+sudo chroot ufs pip3.6 install argh avro-json-serializer avro-python3 \
+     confluent-kafka gevent greenlet kazoo netifaces pathtools pip \
+     prometheus-client pykafka PyYAML quickavro requests setuptools \
+     simplejson six tabulate tc-bbn-py watchdog
 
 cat <<EOF | sudo tee ufs/etc/fstab
 # Device        Mountpoint      FStype  Options Dump    Pass#
@@ -37,6 +46,8 @@ ntpd_enable="YES"
 EOF
 
 sudo rm -f ufs/etc/resolv.conf
+
+sudo chroot ufs pw useradd -n darpa -c "DARPA" -s /bin/sh -m -w yes
 
 sudo makefs -d 6144 -t ffs -f 200000 -s 200g -o version=2,bsize=32768,fsize=4096 -Z ufs.img ufs
 mkimg -s gpt -f qcow2 \
