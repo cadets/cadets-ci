80f4fad7d8bd8b696905af208659caa7a2a35025
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-08-28 17:50:42 +0100

    Increase memory size for test VM

--- scripts/cadets-test.sh
+++ scripts/cadets-test.sh
@@ -26,11 +26,11 @@ truncate -s ${TAR_FILE_SIZE} ${TAR_FILE}
 # run test VM image with bhyve
 TEST_VM_NAME=${JOB_NAME}-${BUILD_NUMBER}
 sudo /usr/sbin/bhyvectl --vm=${TEST_VM_NAME} --destroy || true
-sudo /usr/sbin/bhyveload -c stdio -m 4096m -d ${IMG_NAME} ${TEST_VM_NAME}
+sudo /usr/sbin/bhyveload -c stdio -m 8192m -d ${IMG_NAME} ${TEST_VM_NAME}
 set +e
 expect -c "set timeout 7140; \
 	spawn sudo /usr/bin/timeout -k 60 7020 /usr/sbin/bhyve \
-	-c 2 -m 4096m -A -H -P -g 0 \
+	-c 2 -m 8192m -A -H -P -g 0 \
 	-s 0:0,hostbridge \
 	-s 1:0,lpc \
 	-s 2:0,ahci-hd,${IMG_NAME} \
