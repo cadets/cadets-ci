42022b49c33b16d47fdba6c66a1da8b0fc95ac14
Silviu Chiricescu <silviu.chiricescu@baesystems.com>
2018-10-02 07:03:14 -0400

    * Fixed the user creation command but comment it out in the end as we have chenged the was the user accounts are created (now on the target CADETS machine instead of the host machine)

--- scripts/cadets-bbn-vm.sh
+++ scripts/cadets-bbn-vm.sh
@@ -16,8 +16,8 @@ sudo ${INSTALL} ${CONFIG}/fstab ufs/etc/
 sudo ${INSTALL} ${CONFIG}/loader.conf ufs/boot/
 sudo ${INSTALL} ${CONFIG}/rc.conf ufs/etc/
 
-sudo chroot ufs echo "starc" pw useradd -n darpa -c "DARPA" -s /bin/sh -m -h 0
-sudo chroot ufs echo "starc" pw useradd -n bbn -c "BBN" -s /bin/sh -m -h 0
+# sudo chroot ufs echo "starc" | pw useradd -n darpa -c "DARPA" -s /bin/sh -m -h 0
+# sudo chroot ufs echo "starc" | pw useradd -n bbn -c "BBN" -s /bin/sh -m -h 0
 
 # bhyve does not currently support qcow2 format, so create "raw" format instead
 # build_image cadets-bbn-vm.qcow2 ufs qcow2 200g 1000000
